c17a6ec1e5ac3f1e42aa6873ecffe37d56b466c9
Stéphane K <steffkarraz@hotmail.com>
2021-12-06 18:14:10 +0100

    day01

---
+++ src/bin/day01.rs
@@ -0,0 +1,49 @@
+use std::fs::File;
+use std::io::{ BufReader, BufRead, Error};
+
+fn ex1() -> Result<(), Error> {
+    let input = File::open("/home/skarraz/Projects/AdventOfCode2021/src/bin/day01/input.txt")?;
+    let buffered = BufReader::new(input);
+    let mut count = 0;
+    let mut prev_nb = 10000000;
+
+    for line in buffered.lines() {
+        let nb = line?.parse::<i32>().unwrap_or(0);
+        if nb > prev_nb {
+            count += 1;
+        }
+        prev_nb = nb;
+       
+    }
+    println!("{}", count);
+    Ok(())
+
+}
+
+
+fn ex2() -> Result<(), Error> {
+    let input = File::open("/home/skarraz/Projects/AdventOfCode2021/src/bin/day01/input.txt")?;
+    let buffered = BufReader::new(input);
+    let mut prev_nbs: std::vec::Vec<i32> = vec![];
+    let mut count = 0;
+
+    for line in buffered.lines() {
+        let nb = line?.parse::<i32>().unwrap_or(0);
+        if prev_nbs.len() >= 3 {
+            let prev_sum: i32 = prev_nbs[0] + prev_nbs[1] + prev_nbs[2];
+            let actual_sum = nb +  prev_nbs[1] + prev_nbs[2];
+            if prev_sum < actual_sum {
+                count += 1;
+            }
+            prev_nbs.drain(0..1);
+        }
+        prev_nbs.push(nb);
+    }
+    println!("{}", count);
+    Ok(())
+}
+
+fn main()-> Result<(), Error> {
+    ex2()
+}
+
